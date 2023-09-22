b61a4675161b0b1b91d140251153572196db96ea
Amina <kannami2004@gmail.com>
2023-09-22 22:03:41 +0800

    done hw

--- 6_march/trash_delete.py
+++ 6_march/trash_delete.py
@@ -3,7 +3,7 @@ import re
 
 
 if __name__ == '__main__':
-    for (path, _, filenames) in os.walk('.'):
+    for (path, _, filenames) in os.walk('6_march'):
         for filename in filenames:
             if re.match(r'^trash\..+$', filename):
                 os.remove(os.path.join(path, filename))
